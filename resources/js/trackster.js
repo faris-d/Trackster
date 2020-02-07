809dcbc470be18d198a919d748ef368c20f06bc4
Faris Dweik <farisdweik@gmail.com>
2020-02-07 00:45:54 -0500

    missing rendered images

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,10 +1,17 @@
 const API_KEY = "b74bc84df5f10003bfb924544b6d625c";
 
 var Trackster = {};
+
 $(document).ready(function(){
   $("#search-button").click(function(){
     let input = $("#search-input").val();
     Trackster.searchTracksByTitle(input);
+    $.ajax({
+      url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + input + "&api_key=" + API_KEY + "&format=json",
+      success: function (result) {
+        console.log('success', result);
+      }
+    });
   });
 });
 
@@ -14,7 +21,23 @@ $(document).ready(function(){
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
-
+  const $trackList = $('#track-list');
+  $trackList.empty();
+  for (let i=0; i<tracks.length; i++){
+    let mediumAlbumArt = tracks[i].image[1]["#text"];
+    let htmlTrackRow =
+      '<tr>' +
+      '  <td scope="row">' +
+      '    <a href="' + tracks[i].url + '">'+
+      '       <i class="fa fa-play-circle-o" aria-hidden="true"></i>'+
+      '  </td>'+
+      '  <td>' + tracks[i].name + '</td>'+
+      '  <td>' + tracks[i].artist + '</td>'+
+      '  <td><img src="' + mediumAlbumArt + '"></td>'+
+      '  <td>' + tracks[i].listeners + '</td>'+
+      '</tr>';
+      $trackList.append(htmlTrackRow);
+  }
 };
 
 /*
@@ -25,8 +48,8 @@ Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     type: 'GET',
     url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_KEY + "&format=json",
-    success: function(result){
-      console.log('success', result);
+    success: function (result) {
+      Trackster.renderTracks(result.results.trackmatches.track);
     }
   });
 };
